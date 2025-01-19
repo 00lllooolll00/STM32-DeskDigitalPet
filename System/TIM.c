ef7a43fbf6405324b13828c71eb690e1fd990cb7
NintyNine99 <2570955716@qq.com>
2025-01-19 20:44:23 +0800

    FInish TIM part

--- System/TIM.c
+++ System/TIM.c
@@ -2,7 +2,118 @@
     TIM2 --> 四肢
     TIM3 --> 尾巴
 */
-
 #include "TIM.h"
 
+/**
+ * @brief 定时器PWM模式初始化
+ * 
+ */
+void TIM_Init(void)
+{
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2,ENABLE);
+    RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3,ENABLE);
 
+    GPIO_InitTypeDef GPIOA_PWM_Init_Struct;
+    GPIOA_PWM_Init_Struct.GPIO_Mode = GPIO_Mode_AF_PP;
+    GPIOA_PWM_Init_Struct.GPIO_Pin = AllBody;
+    GPIOA_PWM_Init_Struct.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA,&GPIOA_PWM_Init_Struct);
+
+    TIM_InternalClockConfig(TIM2);
+    TIM_InternalClockConfig(TIM3);
+
+    /*TIM2********************************************/
+    TIM_TimeBaseInitTypeDef TIM2_PWM_Init_Struct;
+    TIM_TimeBaseStructInit(&TIM2_PWM_Init_Struct);
+    TIM2_PWM_Init_Struct.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM2_PWM_Init_Struct.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM2_PWM_Init_Struct.TIM_Period = 20000 - 1;
+    TIM2_PWM_Init_Struct.TIM_Prescaler = 72 - 1;
+    TIM_TimeBaseInit(TIM2,&TIM2_PWM_Init_Struct);
+
+    TIM_OCInitTypeDef TIM2_OC_Init_Struct;
+    TIM_OCStructInit(&TIM2_OC_Init_Struct);
+    TIM2_OC_Init_Struct.TIM_OCMode = TIM_OCMode_PWM1;
+    TIM2_OC_Init_Struct.TIM_OCPolarity = TIM_OCPolarity_High;
+    TIM2_OC_Init_Struct.TIM_OutputState = TIM_OutputState_Enable;
+    TIM2_OC_Init_Struct.TIM_Pulse = 0;
+    TIM_OC1Init(TIM2,&TIM2_OC_Init_Struct);
+    TIM_OC2Init(TIM2,&TIM2_OC_Init_Struct);
+    TIM_OC3Init(TIM2,&TIM2_OC_Init_Struct);
+    TIM_OC4Init(TIM2,&TIM2_OC_Init_Struct);
+    /********************************************TIM2*/
+
+    /*TIM3********************************************/
+    TIM_TimeBaseInitTypeDef TIM3_PWM_Init_Struct;
+    TIM_TimeBaseStructInit(&TIM3_PWM_Init_Struct);
+    TIM3_PWM_Init_Struct.TIM_ClockDivision = TIM_CKD_DIV1;
+    TIM3_PWM_Init_Struct.TIM_CounterMode = TIM_CounterMode_Up;
+    TIM3_PWM_Init_Struct.TIM_Period = 20000 - 1;
+    TIM3_PWM_Init_Struct.TIM_Prescaler = 72 - 1;
+    TIM_TimeBaseInit(TIM3,&TIM3_PWM_Init_Struct);
+
+    TIM_OCInitTypeDef TIM3_OC_Init_Struct;
+    TIM_OCStructInit(&TIM3_OC_Init_Struct);
+    TIM3_OC_Init_Struct.TIM_OCMode = TIM_OCMode_PWM1;
+    TIM3_OC_Init_Struct.TIM_OCPolarity = TIM_OCPolarity_High;
+    TIM3_OC_Init_Struct.TIM_OutputState = TIM_OutputState_Enable;
+    TIM3_OC_Init_Struct.TIM_Pulse = 0;
+    TIM_OC1Init(TIM3,&TIM3_OC_Init_Struct);
+    /********************************************TIM3*/
+
+    TIM_Cmd(TIM2,ENABLE);
+    TIM_Cmd(TIM3,ENABLE);
+}
+
+/**
+ * @brief 设置左前腿姿态
+ * 
+ * @param Compare 姿态值
+ */
+void Set_LFLeg(uint16_t Compare)
+{
+    TIM_SetCompare1(TIM2,Compare);
+}
+
+/**
+ * @brief 设置右前腿姿态
+ * 
+ * @param Compare 姿态值
+ */
+void Set_LRLeg(uint16_t Compare)
+{
+    TIM_SetCompare2(TIM2,Compare);
+}
+
+/**
+ * @brief 设置左后腿姿态
+ * 
+ * @param Compare 姿态值
+ */
+
+void Set_HLLeg(uint16_t Compare)
+{
+    TIM_SetCompare3(TIM2,Compare);
+}
+
+/**
+ * @brief 设置右后腿姿态
+ * 
+ * @param Compare 姿态值
+ */
+
+void Set_HRLeg(uint16_t Compare)
+{
+    TIM_SetCompare4(TIM2,Compare);
+}
+
+/**
+ * @brief 设置尾巴摇动
+ * 
+ * @param Compare 姿态值
+ */
+void Ser_Tail(uint16_t Compare)
+{
+
+}
--- System/TIM.h
+++ System/TIM.h
@@ -3,4 +3,19 @@
 
 #include "stm32f10x.h"
 
+#define LFLeg       GPIO_Pin_0//左前腿
+#define RFLeg       GPIO_Pin_1//右前腿
+#define LHLeg       GPIO_Pin_2//左后腿
+#define RHLeg       GPIO_Pin_3//右后腿
+#define Tail        GPIO_Pin_6//尾巴
+#define AllBody     GPIO_Pin_0|GPIO_Pin_2|GPIO_Pin_1|GPIO_Pin_3|GPIO_Pin_6
+
+void TIM_Init(void);
+void Set_LFLeg(uint16_t Compare);
+void Set_LRLeg(uint16_t Compare);
+void Set_HLLeg(uint16_t Compare);
+void Set_HRLeg(uint16_t Compare);
+void Ser_Tail(uint16_t Compare);
+
+
 #endif
